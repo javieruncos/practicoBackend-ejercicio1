777001dff3641cc8f2ac32c6f184846381fae9ec
Javier Uncos <javiersrfc30@gmail.com>
2022-10-25 23:57:55 -0300

    agregamos las rutas y midleware y morga

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import  Express  from "express";
+import cors from "cors"
+import morgan from "morgan";
+import path from "path"
+
+const app = Express();
+
+app.set("port",process.env.PORT || 4002)
+app.listen(app.get("port"),()=>{
+    console.log("estoy en el puerto" + app.get("port"))
+})
+
+
+app.use(cors());
+app.use(Express.json());
+app.use(Express.urlencoded({extended:true}))
+app.use(morgan("dev"))
+app.use(Express.static(path.join(__dirname,"../public")))
